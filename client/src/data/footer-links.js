f93d896c7b425388d7ae7a72b4ef3170da687acd
ronaksharmark77 <ronaksharma.rk77@gmail.com>
2024-08-23 19:41:24 +0530

    client side code commit

---
+++ client/src/data/footer-links.js
@@ -0,0 +1,38 @@
+export const FooterLink2 = [
+  {
+    title: "Subjects",
+    links: [
+      { title: "Computer Science", link: "/computer-science" },
+      { title: "Data Science", link: "/data-science" },
+      { title: "Developer Tools", link: "/developer-tools" },
+      { title: "DevOps", link: "/devops" },
+      { title: "Machine Learning", link: "/machine-learning" },
+      { title: "Mobile Development", link: "/mobile-development" },
+      { title: "Web Development", link: "/web-development" },
+    ],
+  },
+  {
+    title: "Languages",
+    links: [
+      { title: "C++", link: "/c++" },
+      { title: "Go", link: "/go" },
+      { title: "Java", link: "/java" },
+      { title: "JavaScript", link: "/javascript" },
+      { title: "Kotlin", link: "/kotlin" },
+      { title: "PHP", link: "/php" },
+      { title: "Python", link: "/python" },
+    ],
+  },
+  {
+    title: "Career building",
+    links: [
+        {title: "Career paths", link: "/career-paths"},
+        {title: "Career services", link: "/career-services"},
+        {title: "Interview prep", link: "/interview-prep"},
+        {title: "Professional certification", link: "/professional-certification"},
+        {title: "-", link: "/hi"},
+        {title: "Full Catalog", link: "/full-catalog"},
+        {title: "Beta Content", link: "/beta-content"}
+    ]
+  }
+];
